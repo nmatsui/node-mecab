4f510000c7baa5df59c2f3030060b41116e397c7
Koutarou Chikuba <miz404@gmail.com>
2011-10-28 13:21:25 +0900

    fix README

--- test.js
+++
@@ -1,9 +0,0 @@
-var MeCab = require('./build/default/mecab');
-
-//そのまま使えば普通の出力だよん
-var nomal = new MeCab.Tagger();
-console.log(nomal.parse("すもももももももものうち"));
-
-//コンストラクタに引数を渡せばわかち書きとかになるよん
-var wakati = new MeCab.Tagger("-O wakati");
-console.log(wakati.parse("すもももももももものうち"));
