bac156acee8578e66c7a6e40a99274e22b984515
Koutarou Chikuba <miz404@gmail.com>
2011-10-28 13:51:58 +0900

    add mecabify

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-exports.MeCab = require('./build/default/mecab');
+var MeCab = new require('./build/default/mecab');
+exports.MeCab = MeCab;
+
+var nomal = new MeCab.Tagger();
+exports.mecabify = function(text){
+    var row = nomal.parse(text).split("\n");
+    var i = 0;
+    var buf = []
+    while(row[i++] !== "EOS"){
+        var list = row[i].split(/\r\n|\r|,|\t/);
+        buf.push(list);
+    };
+    return buf;
+};
+
